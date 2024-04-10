d4b9fc1177ff5e087bd8833b5cc9f36654447013
Keriann <keri.gannon@pm.me>
2024-04-09 23:19:52 -0500

    add styling

--- src/components/NewComment/index.tsx
+++ src/components/NewComment/index.tsx
@@ -1,17 +1,58 @@
+import React, { useState } from 'react';
 import './newComment.css';
 
 const NewComment = () => {
+  const [name, setName] = useState('');
+  const [comment, setComment] = useState('');
+  const isDisabled = !name.trim() || !comment.trim();
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    // TODO: Handle POST request
+  };
+
+  const handleNameChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setName(event.target.value);
+  };
+
+  const handleCommentChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setComment(event.target.value);
+  };
+
   return (
     <div className="form-container">
-      <p className="centered">Name</p>
-      <form className="new-comment-form">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" />
+      <h2 className="centered">New Comment</h2>
+      <form className="new-comment-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Name<span className="required">*</span>
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
 
-        <label htmlFor="comment">Comment:</label>
-        <textarea id="comment" name="comment"></textarea>
+        <label htmlFor="comment">
+          Comment<span className="required">*</span>
+        </label>
+        <textarea
+          id="comment"
+          name="comment"
+          value={comment}
+          onChange={handleCommentChange}
+          required
+        ></textarea>
 
-        <button type="submit">Comment</button>
+        <button type="submit" disabled={isDisabled}>
+          Comment
+        </button>
       </form>
     </div>
   );
